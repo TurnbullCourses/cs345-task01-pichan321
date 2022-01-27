866bc53722df6cfd4f76338cb6c41304d05fc426
Utcoop <jacksonbckr13@gmail.com>
2022-01-27 16:01:14 -0500

    Updated withdraw() method

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -30,6 +30,9 @@ public class BankAccount {
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
+        if (amount < 0){
+            throw new IllegalArgumentException();
+        }
         if (amount <= balance){
             balance -= amount;
         }
